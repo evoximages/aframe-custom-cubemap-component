1e97df04750b598b6149f4f62839adc5f8117418
Ben Li <benbli@gmail.com>
2018-11-01 17:17:38 -0700

    init

---
+++ README.md
@@ -0,0 +1,70 @@
+## aframe-custom-cubemap-component
+
+A Custom Cubemap component to load Evox Images interior vehicle assets
+
+For [A-Frame](https://aframe.io).
+
+  - [API](#api)
+  - [Installation](#installation)
+    - [Browser](#browser)
+  - [Usage](#usage)
+    - [Locally hosted images](#locally-hosted-images)
+    - [API fetched images](#api-fetched-images)
+### API
+
+| Property     | Description                                                             | Default Value                                            | Options             |
+| ------------ | ----------------------------------------------------------------------- | -------------------------------------------------------- | ------------------- |
+| folder       | Path to local asset folder                                              | ' '                                                      |                     |
+| edgeLength   | Controls the dimensions of the skybox                                   | 500                                                      |                     |
+| ext          | The image extension                                                     | png                                                      | jpg / png           |
+| transparent  | Toggle transparency for png                                             | false                                                    | true / false        |
+| stereo       | Controls the layer on which the image is rendered.                      | both                                                     | both / left / right |
+| eye          | Fetch either left or right eye asset (*API use only)                    | \_L_                                                     | \_L_  / \_R_        |
+| vif          | Vehicle vifnum *API use only                                            | none                                                     |                     |
+| key          | API key **API use only                                                  | none                                                     |                     |
+| localSrcUrls | Local image naming scheme that maps to the skybox. **Order is important | `['1.png', '3.png', '4.png', '5.png', '0.png', '2.png']` |                     |
+
+### Installation
+
+#### Browser
+
+Install and use by directly including the [browser files](dist):
+
+### Usage
+
+#### Locally hosted images
+```html
+<head>
+  <title>My A-Frame Scene</title>
+  <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
+  <script src="aframe-custom-cubemap-component.min.js"></script>
+</head>
+
+<body>
+  <a-scene rotation="0 180 0" vr-mode-ui="enabled: false"> <!-- Rotate the scene to adjust for a flipped bottom image-->
+    <a-entity custom-cubemap="folder:media/interior/test/; transparent:true;"></a-entity>
+  </a-scene>
+</body>
+```
+![screen1](public/screen1.png)
+![screen2](public/screen2.png)
+![screen3](public/screen3.png)
+
+#### API fetched images
+```html
+<head>
+  <title>My A-Frame Scene</title>
+  <script src="https://aframe.io/releases/0.8.2/aframe.min.js"></script>
+  <script src="aframe-custom-cubemap-component.min.js"></script>
+</head>
+
+<body>
+  <a-scene rotation="0 180 0" vr-mode-ui="enabled: false"> <!-- Rotate the scene to adjust for a flipped bottom image-->
+    <a-entity custom-cubemap="vif:12626; key:{client_api_key}; eye:_L_; transparent:true;"></a-entity>
+  </a-scene>
+</body>
+```
+![screen4](public/screen4.png)
+![screen5](public/screen5.png)
+![screen6](public/screen6.png)
+
---
+++ index.js
@@ -0,0 +1,208 @@
+/* global AFRAME, THREE */
+if (typeof AFRAME === 'undefined') {
+  throw new Error(
+    'Component attempted to register before AFRAME was available.'
+  );
+}
+
+/**
+ * Custom Cubemap component for A-Frame.
+ */
+AFRAME.registerComponent('custom-cubemap', {
+  multiple: true,
+  schema: {
+    folder: {
+      type: 'string',
+      default: '',
+    },
+    edgeLength: {
+      type: 'int',
+      default: 500,
+    },
+    ext: {
+      type: 'string',
+      default: 'png',
+    },
+    transparent: {
+      type: 'boolean',
+      default: false,
+    },
+    formatRGBA: {
+      type: 'boolean',
+      default: true,
+    },
+    stereo: {
+      type: 'string',
+      default: 'both',
+    },
+    eye: {
+      type: 'string',
+      default: '_L_',
+    },
+    vif: {
+      type: 'string',
+      default: '',
+    },
+    key: {
+      type: 'string',
+      default: '',
+    },
+    localSrcUrls: {
+      type: 'array',
+      default: ['1.png', '3.png', '4.png', '5.png', '0.png', '2.png'],
+    },
+  },
+
+  init() {
+    this.onEnterVr = this.onEnterVr.bind(this);
+    this.onExitVr = this.onExitVr.bind(this);
+    this.setStereoLayer = this.setStereoLayer.bind(this);
+  },
+
+  update() {
+    const data = this.data;
+
+    // determine local vs api as source for cubemap images
+    if (data.folder !== '') {
+      cubemap = this.loadCubemapTexture(data.localSrcUrls, data.folder);
+      return this.createSkyBox(cubemap);
+    }
+
+    if (data.vif !== '') {
+      let response = this.fetchData();
+      response.then(rawData => this.sortAndFilterImages(rawData));
+    }
+  },
+
+  play() {
+    this.addEventListeners();
+  },
+
+  remove() {
+    this.removeEventListeners();
+    this.el.removeObject3D('cubemap');
+  },
+
+  onEnterVr() {
+    this.setStereoLayer('inVrMode');
+  },
+
+  onExitVr() {
+    this.setStereoLayer('outVrMode');
+  },
+
+  addEventListeners() {
+    const canvasEl = document.querySelector('a-scene');
+    if (canvasEl) {
+      canvasEl.addEventListener('enter-vr', this.onEnterVr, false);
+      canvasEl.addEventListener('exit-vr', this.onExitVr, false);
+    }
+  },
+
+  removeEventListeners() {
+    const canvasEl = document.querySelector('a-scene');
+    if (canvasEl) {
+      canvasEl.removeEventListener('enter-vr', this.onEnterVr);
+      canvasEl.removeEventListener('exit-vr', this.onExitVr);
+    }
+  },
+
+  setStereoLayer(mode) {
+    const data = this.data;
+    const obj3D = this.el.object3D.children[0];
+
+    if (data.stereo === 'both' || data.stereo === 'left') {
+      obj3D.layers.set(0);
+    } else if (data.stereo === 'right') {
+      obj3D.layers.set(2);
+    }
+
+    if (mode === 'inVrMode' && data.stereo === 'left') {
+      obj3D.layers.set(1);
+    }
+  },
+
+  createSkyBox(cubemap) {
+    const shader = THREE.ShaderLib.cube;
+
+    // Create shader material
+    const skyBoxShader = new THREE.ShaderMaterial({
+      fragmentShader: shader.fragmentShader,
+      vertexShader: shader.vertexShader,
+      transparent: this.data.transparent,
+      uniforms: shader.uniforms,
+      side: THREE.BackSide,
+      depthWrite: false,
+    });
+
+    // Clone ShaderMaterial (necessary for multiple cubemaps)
+    const skyBoxMaterial = skyBoxShader.clone();
+
+    // Apply cubemap textures to shader uniforms
+    skyBoxMaterial.uniforms.tCube.value = cubemap;
+
+    // Set skybox dimensions
+    const edgeLength = this.data.edgeLength;
+    const skyBoxGeometry = new THREE.CubeGeometry(
+      edgeLength,
+      edgeLength,
+      edgeLength
+    );
+
+    // Set entity's object3D
+    this.el.setObject3D(
+      'cubemap',
+      new THREE.Mesh(skyBoxGeometry, skyBoxMaterial)
+    );
+  },
+
+  loadCubemapTexture(urls, path) {
+    const loader = new THREE.CubeTextureLoader();
+    if (path !== '') {
+      loader.setPath(path);
+    }
+    return loader.load(urls);
+  },
+
+  sortAndFilterImages(jsonData) {
+    let proxy = 'https://cors-anywhere.herokuapp.com/';
+    let filteredArray = [];
+    let sortedArray = [];
+    let imgUrls;
+    filteredArray = jsonData.urls.filter(url =>
+      RegExp(this.data.eye).test(url)
+    );
+    sortedArray = filteredArray.sort();
+
+    // THREE.js loader array img order
+    imgUrls = [
+      proxy + sortedArray[1], // right
+      proxy + sortedArray[3], // left
+      proxy + sortedArray[4], // top
+      proxy + sortedArray[5], // bottom
+      proxy + sortedArray[0], // front
+      proxy + sortedArray[2], // back
+    ];
+    let cubemap = this.loadCubemapTexture(imgUrls, '');
+    this.createSkyBox(cubemap);
+  },
+
+  async fetchData() {
+    let opt = {
+      vif: this.data.vif,
+      apiKey: '?api_key=' + this.data.key,
+      product: '/products/14/134',
+      base:
+        'http://vehicles-api-dev.us-west-2.elasticbeanstalk.com/api/v1/vehicles/',
+    };
+
+    const fetchUrl = opt.base + opt.vif + opt.product + opt.apiKey;
+
+    return await fetch(fetchUrl)
+      .then(this.handleErrors)
+      .then(response => response.json())
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+});
